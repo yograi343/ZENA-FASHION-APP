8fb39a0aca573c7c281f3d161b3acbfc5ba12c66
Yogesh Rai <63515909+yograi343@users.noreply.github.com>
2024-03-12 18:12:26 +0530

    image added

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -1,6 +1,7 @@
 #import the libraries required
 import streamlit as st
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 st.title("Zena's Amazing Athleisure Catalog")
 st.write('Pick a sweatshirt color or style')
@@ -10,9 +11,13 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 
 # load data
-my_dataframe = session.table('ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE').select(col('COLOR_OR_STYLE'))
+my_dataframe = session.table('ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG_FOR_WEBSITE').select(col('COLOR_OR_STYLE'),col('DIRECT_URL'))
 
 
-st.selectbox( 'Select a style of Color',
-              my_dataframe,
-              index = None)
+selected_style = st.selectbox( 'Select a style of Color',
+                    my_dataframe,
+                    index = None)
+if selected_style:
+  pd_df = my_dataframe.to_pandas()
+  pd_df = pd_df[pd_df.loc[pd_df['COLOR_OR_STYLE']==selected_style,'DIRECT_URL'].iloc(0)
+  st.image(pd_df)
